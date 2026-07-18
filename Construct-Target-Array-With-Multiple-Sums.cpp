aa77fba871b2bb47ae94c37e3aa9220964d456ea
Joao-Daniel2008 <jdmm@cin.ufpe.br>
2026-07-18 18:19:53 -0300

    feat: add solution for Construct-Target-Array-With-Multiple-Sums.cpp

---
+++ Construct-Target-Array-With-Multiple-Sums.cpp
@@ -0,0 +1,14 @@
+        long long sum2;
+        int num;
+
+        }
+            heap.push(target[i]);
+            sum += target[i];
+        for(int i = 0; i < length; i++){
+        int length = target.size();
+        long long sum = 0;
+        priority_queue<int, vector<int>, less<int>> heap;
+        bool isPossible = false;
+    bool isPossible(vector<int>& target) {
+public:
+class Solution {
